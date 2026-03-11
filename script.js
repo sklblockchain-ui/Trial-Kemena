6560e274744d6cc80da297cd8fa66b658970e90d
sklblockchain-ui <sklblockchain@gmail.com>
2026-03-12 01:09:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,104 +1,29 @@
-function loadTable(file,element){
+function loadPopulation(){
 
-Papa.parse(file,{
-
-download:true,
-header:true,
-
-complete:function(results){
-
-let data = results.data
-
-let html = "<table>"
-
-Object.keys(data[0]).forEach(col=>{
-html+="<th>"+col+"</th>"
-})
-
-data.forEach(row=>{
-
-html+="<tr>"
-
-Object.values(row).forEach(val=>{
-html+="<td>"+val+"</td>"
-})
-
-html+="</tr>"
-
-})
-
-html+="</table>"
-
-document.getElementById(element).innerHTML=html
-
-}
-
-})
-
-}
-
-loadTable("data/education.csv","educationTable")
-loadTable("data/facilities.csv","facilitiesTable")
-loadTable("data/health.csv","healthTable")
-loadTable("data/safety.csv","safetyTable")
-loadTable("data/income.csv","incomeTable")
-
-Papa.parse("data/ethnicity.csv",{
+Papa.parse("data/population.csv",{
 
 download:true,
 header:true,
 
 complete:function(results){
 
-let labels=[]
-let values=[]
+let html=""
 
-results.data.forEach(r=>{
-labels.push(r.ethnicity)
-values.push(r.percentage)
+results.data.forEach(row=>{
+
+html += `
+<div class="stat">
+<h3>${row.indicator}</h3>
+<p>${row.value}</p>
+</div>
+`
+
 })
 
-new Chart(document.getElementById("ethnicityChart"),{
+document.getElementById("population").innerHTML = html
 
-type:'doughnut',
-
-data:{
-labels:labels,
-datasets:[{data:values}]
 }
 
 })
 
 }
-
-})
-
-Papa.parse("data/age_structure.csv",{
-
-download:true,
-header:true,
-
-complete:function(results){
-
-let labels=[]
-let values=[]
-
-results.data.forEach(r=>{
-labels.push(r.age_group)
-values.push(r.percentage)
-})
-
-new Chart(document.getElementById("ageChart"),{
-
-type:'bar',
-
-data:{
-labels:labels,
-datasets:[{data:values}]
-}
-
-})
-
-}
-
-})
